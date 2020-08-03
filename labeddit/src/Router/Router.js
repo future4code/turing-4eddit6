8fbcdf13b8e71db29fd6a9c9fde995c3da767037
Dienay Lima <dienaylima@gmail.com>
2020-08-03 14:42:47 -0300

    Router

--- labeddit/src/Router/Router.js
+++ labeddit/src/Router/Router.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Login from '../Components/Login/Login';
+import SingIn from '../Components/SingIn/SingIn';
+import Feed from '../Components/Feed/Feed';
+
 
 // import { Container } from './styles';
 
 function Router() {
   return  (
-      <Browse
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" >
+                <Login />
+            </Route>
+
+            <Route exact path="/login" >
+                <SingIn />
+            </Route>
+
+            <Route exact path="/create" >
+                <Feed />
+            </Route>
+
+            <Route path="/" >
+                <h3>Eita Giovana, o forninho caiu (404)</h3>
+            </Route>
+        </Switch>
+    </BrowserRouter>
   )
 }
 
